1d282a391546c17ca361ffd688b782a5a759b5fb
edqmzmcn <fernando.o.pina@gmail.com>
2020-10-01 00:20:42 -0300

    Login screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,96 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: 'Flutter Tutorial',
-    home: MyButton(),
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(primaryColor: Colors.yellow),
+    title: 'Sistema Especialista de Manutenção',
+    home: TelaLogin(),
   ));
 }
 
-class MyButton extends StatelessWidget {
+//
+// TELA DE LOGIN
+//
+class TelaLogin extends StatefulWidget {
+  @override
+  _TelaLoginState createState() => _TelaLoginState();
+}
+
+class _TelaLoginState extends State<TelaLogin> {
+  GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+  
+  // Variáveis
+  TextEditingController txtUsuario = TextEditingController();
+  TextEditingController txtSenha = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        print('MyButton was tapped!');
-      },
-      child: Container(
-        height: 36.0,
-        padding: const EdgeInsets.all(8.0),
-        margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5.0),
-          color: Colors.lightGreen[500],
-        ),
-        child: Center(
-          child: Text('Engage'),
-        ),
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text("Login"),
       ),
+
+      body: SingleChildScrollView(
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+            key: _formkey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.people, size: 80, color: Colors.black),
+                campoTexto("Usuário", txtUsuario),
+                campoTexto("Senha", txtSenha),
+                botaoLogar(context),
+              ],
+            ),
+          ),
+        ),
+
+      backgroundColor: Colors.white,
     );
   }
+}
+
+//
+// CAMPOS DE TEXTO (TELA LOGIN)
+//
+campoTexto(rotulo, controle){
+  return Container(
+    padding: const EdgeInsets.symmetric(vertical: 5),
+    child: TextFormField(
+      keyboardType: TextInputType.text,
+      style: TextStyle(fontSize: 25),
+      decoration: InputDecoration(
+        labelText: rotulo,
+        labelStyle: TextStyle(fontSize: 15, color: Colors.black),
+      ),
+      controller: controle,
+      validator: (value) {
+        return (value.isEmpty) ? "Usuário ou senha incorreto": null;
+      }
+    ),
+  );
+}
+
+//
+// BOTÃO LOGAR
+//
+botaoLogar(BuildContext context){
+  return Container(
+    padding: const EdgeInsetsDirectional.only(top: 20),
+    child: RaisedButton(
+      child: Text(
+        "Login",
+        style: TextStyle(
+          color: Colors.red,
+          fontSize: 18,
+        ),
+      ),
+      color: Colors.yellow,
+      onPressed: () {
+
+      },
+    )
+  );
 }

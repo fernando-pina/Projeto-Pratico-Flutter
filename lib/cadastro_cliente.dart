75c1c577e25d6df6881736df977ac23bcd436a8d
edqmzmcn <fernando.o.pina@gmail.com>
2020-10-23 05:54:12 -0300

    Inserido os formularios de 'CADASTRO DE CLIENTES' e 'ORDEM DE SERVIÇO'

--- lib/cadastro_cliente.dart
+++ lib/cadastro_cliente.dart
@@ -1,5 +1,5 @@
 //
-// TELA DE CADASTRO DE CLIENTE
+// TELA DE CADASTRO DE CLIENTES
 //
 import 'package:flutter/material.dart';
 
@@ -9,14 +9,49 @@ class CadastroClientes extends StatefulWidget {
 }
 
 class _CadastroClientesState extends State<CadastroClientes> {
-  
-  //String msg = "";
+  GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+
+  TextEditingController txtNome = TextEditingController();
+  TextEditingController txtTelefone = TextEditingController();
+  TextEditingController txtEndereco = TextEditingController();
+  TextEditingController txtCidade = TextEditingController();
+  TextEditingController txtEstado = TextEditingController();
+  TextEditingController txtCEP = TextEditingController();
+  TextEditingController txtMarcaDaMaquina = TextEditingController();
+  TextEditingController txtNumeroSerie = TextEditingController();
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Cadastro de Clientes")),
-      body:Center(child: Text("Formulários...")),
+      appBar: AppBar(
+        title: Text("Cadastro de Clientes")
+      ),
+      body:SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.people, size: 80),
+                  Text("Clientes", style: TextStyle(fontSize: 30)),
+                ],
+              ),
+              campoTexto("Nome", txtNome),
+              campoTexto("Telefone", txtTelefone),
+              campoTexto("Endereco", txtEndereco),
+              campoTexto("Cidade", txtCidade),
+              campoTexto("Estado", txtEstado),
+              campoTexto("CEP", txtCEP),
+              campoTexto("Marca da Máquina", txtMarcaDaMaquina),
+              campoTexto("Número de Série", txtNumeroSerie),
+            ]
+          ),
+        ),
+      ),
       bottomNavigationBar: BottomAppBar(
         child: Container(
           child: Text("Sistema Especialista em Manutenção"),
@@ -25,4 +60,22 @@ class _CadastroClientesState extends State<CadastroClientes> {
       )
     );
   }
+}
+
+campoTexto(rotulo, controle){
+  return Container(
+    padding: const EdgeInsets.symmetric(vertical: 5),
+    child: TextFormField(
+      keyboardType: TextInputType.text,
+      style: TextStyle(fontSize: 20),
+      decoration: InputDecoration(
+        labelText: rotulo,
+        labelStyle: TextStyle(fontSize: 12),
+      ),
+      controller: controle,
+      validator: (value){
+        return (value.isEmpty) ? "É obrigatório o preenchimento de todos os campos." : null;
+      },
+    ),
+  );
 }
--- lib/ordem_servico.dart
+++ lib/ordem_servico.dart
@@ -9,14 +9,67 @@ class OrdemServico extends StatefulWidget {
 }
 
 class _OrdemServicoState extends State<OrdemServico> {
-  
-  //String msg = "";
+  GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+
+  TextEditingController txtOS = TextEditingController();
+  TextEditingController txtNomeDoConcessionario = TextEditingController();
+  TextEditingController txtNomeCliente = TextEditingController();
+  TextEditingController txtTelefoneCliente = TextEditingController();
+  TextEditingController txtEnderecoCliente = TextEditingController();
+  TextEditingController txtCidadeCliente = TextEditingController();
+  TextEditingController txtEstadoCliente = TextEditingController();
+  TextEditingController txtCEPCliente = TextEditingController();
+  TextEditingController txtMarcaDaMaquina = TextEditingController();
+  TextEditingController txtNumeroSerie = TextEditingController();
+  TextEditingController txtOcorrencia = TextEditingController();
+  TextEditingController txtCausasProvaveis = TextEditingController();
+  TextEditingController txtAcaoTomada = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Abertura de Ordem de Serviços")),
-      body:Center(child: Text("Formulários / Botão ENVIAR/ Botão ???")),
+      body:SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.description, size: 80),
+                  Text("Ordem de Serviço", style: TextStyle(fontSize: 30)),
+                ],
+              ),
+              campoTexto("OS", txtOS),
+              SizedBox(height: 50),
+              Text("DADOS DO SERVEIÇO TÉCNICO AUTORIZADO", style: TextStyle(fontStyle: FontStyle.italic)),
+              campoTexto("Nome do Concessionário", txtNomeDoConcessionario),
+              SizedBox(height: 50),
+              Text("DADOS DO CLIENTE", style: TextStyle(fontStyle: FontStyle.italic)),
+              campoTexto("Nome", txtNomeCliente),
+              campoTexto("Telefone", txtTelefoneCliente),
+              campoTexto("Endereco", txtEnderecoCliente),
+              campoTexto("Cidade", txtCidadeCliente),
+              campoTexto("Estado", txtEstadoCliente),
+              campoTexto("CEP", txtCEPCliente),
+              SizedBox(height: 50),
+              Text("IDENTIFICAÇÃO DO EQUIPAMENTO", style: TextStyle(fontStyle: FontStyle.italic)),
+              campoTexto("Marca da Máquina", txtMarcaDaMaquina),
+              campoTexto("Número de Série", txtNumeroSerie),
+              SizedBox(height: 50),
+              Text("REGISTRO DA SOLICITAÇÃO", style: TextStyle(fontStyle: FontStyle.italic)),
+              campoTexto("Ocorrência", txtOcorrencia),
+              SizedBox(height: 50),
+              Text("FALHAS OU OBSERVAÇÕES", style: TextStyle(fontStyle: FontStyle.italic)),
+              campoTexto("Causas Prováveis", txtCausasProvaveis),
+              campoTexto("Ação Tomada", txtAcaoTomada),
+            ]
+          ),
+        ),
+      ),
       bottomNavigationBar: BottomAppBar(
         child: Container(
           child: Text("Sistema Especialista em Manutenção"),
@@ -25,4 +78,23 @@ class _OrdemServicoState extends State<OrdemServico> {
       )
     );
   }
+}
+
+
+campoTexto(rotulo, controle){
+  return Container(
+    padding: const EdgeInsets.symmetric(vertical: 5),
+    child: TextFormField(
+      keyboardType: TextInputType.text,
+      style: TextStyle(fontSize: 20),
+      decoration: InputDecoration(
+        labelText: rotulo,
+        labelStyle: TextStyle(fontSize: 12),
+      ),
+      controller: controle,
+      validator: (value){
+        return (value.isEmpty) ? "É obrigatório o preenchimento de todos os campos." : null;
+      },
+    ),
+  );
 }
